bf4279ee490c817399a89fe922371d6a8d7c980c
radeshshiwlochan <radesh0430@gmail.com>
2017-09-18 12:26:48 -0400

    fixes formatting

--- getClosingParen.cpp
+++ getClosingParen.cpp
@@ -6,11 +6,11 @@ int getClosingParen(std::string sentence, int position )
     std::vector<int> openParenPos;
     for ( int index = position; index < sentence.length(); ++index )
     {
-    	if ( sentence[index] == '(' ) 
+        if ( sentence[index] == '(' ) 
     		openParenPos.push_back(index);
-    	else if ( sentence[index] == ')' && openParenPos.size() > 1 )
+    	 else if ( sentence[index] == ')' && openParenPos.size() > 1 )
             openParenPos.pop_back();
-        else if ( sentence[index] == ')' && openParenPos.size() == 1 )
+         else if ( sentence[index] == ')' && openParenPos.size() == 1 )
         	return index;
     }
     /*
@@ -22,7 +22,7 @@ int getClosingParen(std::string sentence, int position )
 
 int main() 
 {
-	std::string str = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.";
-	std::cout << getClosingParen( str, 0 ) << std::endl;
-	return 0;
+    std::string str = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.";
+    std::cout << getClosingParen( str, 0 ) << std::endl;
+    return 0;
 }
--- stringReformatting.cpp
+++ stringReformatting.cpp
@@ -2,28 +2,28 @@
 
 std::string stringReformatting( std::string str, int groupCount )
 {
-	int counter = 0;
-	std::string reformatStr = "";
-	for ( int i = str.length() - 1; i >= 0; --i )
+    int counter = 0;
+    std::string reformatStr = "";
+    for ( int i = str.length() - 1; i >= 0; --i )
+    {
+        if ( counter == groupCount )
 	{
-		if ( counter == groupCount )
-		{
-			reformatStr = "-" + reformatStr;
-			counter = 0;
-		}
+            reformatStr = "-" + reformatStr;
+	    counter = 0;
+	}
         if ( str[i] != '-' )
         {
-        	reformatStr = str[i] + reformatStr;
-        	counter++;
+            reformatStr = str[i] + reformatStr;
+            counter++;
         }
-	}
-	return reformatStr;	
+    }
+    return reformatStr;	
 }
 
 int main() 
 {
-	std::string s = "2-4a0r7-4k";
-	std::cout << stringReformatting( s, 3 ) << std::endl;
-	std::cout << stringReformatting( s, 4 ) << std::endl;
-	return 0;
+    std::string s = "2-4a0r7-4k";
+    std::cout << stringReformatting( s, 3 ) << std::endl;
+    std::cout << stringReformatting( s, 4 ) << std::endl;
+    return 0;
 }
